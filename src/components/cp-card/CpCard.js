3b8fc7645a1d58bb6b5cfe525b2af7b52d9a1511
Chandan Jha <apple@192.168.1.3>
2023-01-21 00:06:18 +0530

    icons added

--- src/components/cp-card/CpCard.js
+++ src/components/cp-card/CpCard.js
@@ -4,7 +4,7 @@ const CpCard = () => (
   <div className={`${styleCard["cp-card"]} container`}>
     <div className={styleCard["card-item"]}>
       <div className={styleCard["card-lhs"]}>
-        <div className={styleCard["issue-status"]}></div>
+        <div className={styleCard["issue-status"]}><span className='icon-status'></span></div>
         <div className={styleCard["issue-details"]}>
           <h2 className={styleCard["issue-title"]}>
             Bug: broken app when snamespaced tages in JSX
@@ -29,7 +29,7 @@ const CpCard = () => (
           </div>
         </div>
         <div className={styleCard["comment"]}>
-          <span className={styleCard["comment-icon"]}></span>
+          <span className={styleCard["comment-icon"]}><span className="icon-comment"></span></span>
           <span className={styleCard["comment-len"]}>4</span>
         </div>
       </div>
--- src/components/cp-header/seconday-header/SecondayHeader.js
+++ src/components/cp-header/seconday-header/SecondayHeader.js
@@ -5,15 +5,16 @@ const SecondayHeader = () => {
     <div className={`${styleSecondayHeader['cp-sec-header']} container`}>
         <div className={styleSecondayHeader['head-wrap']}>
             <ul  className={styleSecondayHeader['left-wrap']}>
-                <li  className={styleSecondayHeader['status']}></li>
+                <li  className={`${styleSecondayHeader['status']}`}><span className='icon-status'></span></li>
                 <li  className={styleSecondayHeader['status-open']}>625 open</li>
-                <li  className={styleSecondayHeader['status-done']}>10,104 closed</li>
+                <li  className={styleSecondayHeader['status-done']}><span className={`${styleSecondayHeader['icon-done']} icon-done`}></span> 10,104 closed</li>
             </ul>
             <ul  className={styleSecondayHeader['right-wrap']}>
-                <li  className={styleSecondayHeader['nav-link']}>Author</li>
-                <li  className={styleSecondayHeader['nav-link']}>Label</li>
-                <div  className={styleSecondayHeader['nav-link']}>Label</div>
-                <li  className={styleSecondayHeader['nav-link']}>Projects</li>
+                <li  className={`${styleSecondayHeader['nav-link']} `}>Author<span span className={`${styleSecondayHeader['icon-down']} icon-down`}></span></li>
+               
+                <li  className={styleSecondayHeader['nav-link']}>Label<span span className={`${styleSecondayHeader['icon-down']} icon-down`}></span></li>
+                <div  className={styleSecondayHeader['nav-link']}>Projects<span span className={`${styleSecondayHeader['icon-down']} icon-down`}></span></div>
+                <li  className={styleSecondayHeader['nav-link']}>Milestones<span span className={`${styleSecondayHeader['icon-down']} icon-down`}></span></li>
             </ul>
         </div>
     </div>
