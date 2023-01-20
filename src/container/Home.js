574ab058cda2bd2fca638e49a5825b4ce1e15462
Chandan Jha <apple@192.168.1.3>
2023-01-21 02:56:20 +0530

    integrated API

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect, Fragment } from "react";
 import CpCard from "../components/cp-card/CpCard";
 import SecondaryHeader from "../components/cp-header/seconday-header/SecondayHeader";
-import { getIssueData } from "../components/cp-card/cpcard_mock";
+// import { getIssueData } from "../components/cp-card/cpcard_mock";
 const Home = () => {
-  const [data, setData] = useState([{}]);
+  const [data, setData] = useState();
   const [page,setPage] = useState(10);
   
+  const fetchData = ()=>{
+     try {
+        let apiHeader = new Headers();
+        apiHeader.append('Content-Type', 'application/json');
+        fetch(`https://api.github.com/repos/facebook/react/issues`, {
+          method: 'GET',
+          headers: apiHeader,
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            if(result.length>0)
+            setData(result);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    }
   useEffect(() => {
-    setData(getIssueData());
+    fetchData();
   }, []);
 
   useEffect(() => {
@@ -22,14 +42,14 @@ const Home = () => {
     return () => {
       window.removeEventListener('scroll', handleScrollEvent);
     }
-  }, [page])
+  }, [data,page])
  
   return (
     <>
       <SecondaryHeader />
-      {data && data.length > 0 &&
+      {data && data?.length > 0 &&
         data.slice(0,page).map((val) => (
-          <React.Fragment key={val.id}>
+          <React.Fragment key={'K'+val.id}>
             <CpCard
               title={val.title}
               userData={val.user}
